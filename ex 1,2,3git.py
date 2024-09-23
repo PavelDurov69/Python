40ea90a4ff97ad92be387027f18d8a6dbb7c700a
PavelDurov69 <ssvistunov97@gmail.com>
2024-09-23 16:49:19 +0300

    Create ex 1,2,3git.py

---
+++ ex 1,2,3git.py
@@ -0,0 +1,26 @@
+#zadanie 1
+integer_var = 23
+float_var = 2.6
+str_var = "hello!"
+list_var = [1, 2, 3,4]
+tuple_var1 = (23, 33)
+tuple_var2 = ("door", "flat", "window")
+dict_var = {"grang", "game", "home"}
+set_var = (5, 6, 34, 67)
+bool_var = False
+print(integer_var, float_var, str_var, list_var, tuple_var2, tuple_var1, dict_var,set_var, bool_var)
+# zadanie 2
+name = "sanek"
+age = 17
+average_grade = 4.6
+favorite movies = ["Звездные войны", "Братья", "Тихое место"]
+personal_info = ("Гридин", "20", 4.6)
+skills = {"fast : reading", "sociable"}
+visited_countries = { "Germany", "British"}
+has_driver_license = False
+# zadanie 3
+str_var = "intel core i7 12700"
+integer_var = 32
+speed  = 60mbps
+list = ["set_var", "integer_var", "speed"]
+print(list)
